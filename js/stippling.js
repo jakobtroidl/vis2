b0db9b5cb842e90ebf355c4f525a340d8766b1c1
Lukas Herzberger <herzberger.lukas@gmail.com>
2021-04-19 17:45:45 +0200

    implement mach banding

--- js/stippling.js
+++ js/stippling.js
@@ -1,7 +1,158 @@
+/*
+ * Stippling based on Görtler et al. 'Stippling of 2D Scalar Fields'.
+ */
+
+
+/*
+ * Some utility functions.
+ */
+
+/**
+ *
+ * @param val
+ * @param lower
+ * @param upper
+ * @returns {number}
+ */
+const clamp = (val, lower = 0.0, upper = 1.0) => Math.min(upper, Math.max(lower, val));
+const rgbaToLuminance = (r, g, b, _a) => (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255.0;
+const createQuantization = (num) => {
+    const fraction = 1.0 / num;
+    const quantization = [];
+    for (let q = fraction; q < 1.0; q += fraction) {
+        quantization.push(q);
+    }
+    return quantization;
+};
+const quantize = (arr, quantization) => {
+    return arr.map(p => {
+        for (let i = 0; i < quantization.length; ++i) {
+            if (p < quantization[i]) {
+                if (i === 0) {
+                    return 0;
+                } else {
+                    return quantization[i - 1];
+                }
+            }
+        }
+        return 1.0;
+    });
+};
+const floatArrayToImageData = (arr, stride, mapFromMinus1 = false) => {
+    const mapVal = mapFromMinus1 ? p => (p + 1.0) / 2.0 : p => p;
+    return new ImageData(
+        new Uint8ClampedArray(arr.map(p => {
+            const v = clamp(Math.round(mapVal(p) * 255), 0, 255);
+            return [v, v, v, 255];
+        }).flat()),
+        stride,
+        arr.length / stride);
+}
+const imageDataToFloatArray = (imageData, rgbaToFloat = rgbaToLuminance) => {
+    const data = [];
+    let maxValue = 0;
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        const density = rgbaToFloat(
+            imageData.data[i],
+            imageData.data[i+1],
+            imageData.data[i+2],
+            imageData.data[i+3]
+        );
+        data.push(density);
+        if (maxValue < density) {
+            maxValue = density;
+        }
+    }
+    return {data, maxValue};
+}
+const polygonBounds = (points) => {
+    let minX = Infinity;
+    let minY = Infinity;
+    let maxX = 0;
+    let maxY = 0;
+    for (const [x, y] of points) {
+        if (x < minX) {
+            minX = x;
+        }
+        if (x > maxX) {
+            maxX = x;
+        }
+        if (y < minY) {
+            minY = y;
+        }
+        if (y > maxY) {
+            maxY = y;
+        }
+    }
+    return {min: [minX, minY], max: [maxX, maxY]};
+}
+
+/**
+ * Creates a canvas as a child of a div-tag and uses it to present the given {@link ImageData} instance.
+ * @param img the {@link ImageData} to show
+ * @param divName a string, the id of the div
+ */
+const showImage = (img, divName) => {
+    const ctx = d3.select(`#${divName}`)
+        .append('canvas')
+        .attr('width', img.width)
+        .attr('height', img.height)
+        .node()
+        .getContext('2d');
+    ctx.putImageData(img, 0, 0);
+}
+
+/**
+ * Calculates two new cell centroids from a Voronoi cell that should be split.
+ * @param cell
+ * @returns {{position1: *[], position2: *[]}}
+ */
+const splitCell = (cell) => {
+    const centroid = d3.polygonCentroid(cell);
+    let largestDir = [0, 0];
+    let secondLargestDir = [0, 0];
+    let largestDistance = 0;
+    let secondLargestDistance = 0;
+    for (const p of d3.polygonHull(cell)) {
+        const direction = [
+            p[0] - centroid[0],
+            p[1] - centroid[1]
+        ];
+        const squaredDistance = direction.reduce((acc, c) => acc + Math.pow(c, 2), 0);
+        if (squaredDistance > largestDistance) {
+            secondLargestDistance = largestDistance;
+            secondLargestDir = largestDir;
+            largestDistance = squaredDistance;
+            largestDir = direction;
+        }
+    }
+    return {
+        position1: [
+            centroid[0] + largestDir[0] * 0.5,
+            centroid[1] + largestDir[1] * 0.5
+        ],
+        position2: [
+            centroid[0] + secondLargestDir[0] * 0.5,
+            centroid[1] + secondLargestDir[1] * 0.5
+        ]
+    };
+};
+
+
+/*
+ * Helper classes
+ */
+
+/**
+ * A stipple as returned by {@link stipple}.
+ * Has a position (x, y) coordinate with respect to the dimensions of the underlying {@link DensityFunction2D},
+ * and a density ∈ [0,1].
+ */
 class Stipple {
     constructor(x, y) {
         this.setPosition(x, y);
         this.density = 0;
+        this.absoluteDensity = 0;
     }
 
     toArray() {
@@ -24,6 +175,10 @@ class Stipple {
     }
 }
 
+/**
+ * A discrete 2-dimensional density function.
+ * Can be created from an {@link ImageData} instance.
+ */
 class DensityFunction2D {
     /**
      * Note: For performance reasons, if data is a 1d-array it is destroyed in the process of building the density
@@ -32,8 +187,13 @@ class DensityFunction2D {
      * @param stride the width of the 2d function. only needed if data is a 1d-array
      */
     constructor(data, stride = 0) {
-        if (!Array.isArray(data[0])) {
+        if (data instanceof DensityFunction2D) {
+            this.data = data.data;
+            this.width = data.width;
+            this.height = data.height;
+        } else if (!Array.isArray(data[0])) {
             if (!stride) {
+                console.log(data);
                 throw new Error('data is a 1d-array and stride is 0');
             }
             this.width = stride;
@@ -71,74 +231,113 @@ class DensityFunction2D {
     }
 
     /**
-     * Creates a DensityFunction2D from an ImageData instance.
+     * Creates a basic {@link DensityFunction2D} from an {@link ImageData} instance.
      * Pixel data is mapped to a float value and scaled to the range [0,1]
-     * @param imageData an ImageData instance (e.g. from CanvasRenderingContext2D.getImageData()).
-     * @param rgbaToDensity maps 4 positive integers (r,g,b,a) to a float value
+     * @param imageData an {@link ImageData} instance (e.g. from CanvasRenderingContext2D.getImageData()).
+     * @param rgbaToDensity (optional) maps 4 positive integers (r,g,b,a) to a float value
      * @returns {DensityFunction2D}
      */
-    static fromImageData2D(imageData, rgbaToDensity = (r, g, b, _a) => (0.2126 * r + 0.7152 * g + 0.0722 * b) / 255.0) {
-        const data = [];
-        let maxDensity = 0;
-        for (let i = 0; i < imageData.data.length; i += 4) {
-            const density = rgbaToDensity(
-                imageData.data[i],
-                imageData.data[i+1],
-                imageData.data[i+2],
-                imageData.data[i+3]
-            );
-            data.push(density);
-            if (maxDensity < density) {
-                maxDensity = density;
+    static fromImageData2D(imageData, rgbaToDensity = rgbaToLuminance) {
+        const {data, maxValue: maxDensity} = imageDataToFloatArray(imageData);
+        return new DensityFunction2D(data.map(p => p / maxDensity), imageData.width);
+    }
+
+    /**
+     * Creates a {@link DensityFunction2D} with artificial contours from an {@link ImageData} instance.
+     *
+     * The contours are created as follows:
+     *   Φ' = Φ + 2w (C - G_dxd * C - 0.5), if C - G_dxd * C > 0.5
+     *   Φ' = Φ + 2w (C - G_dxd * C - 1.0), if C - G_dxd * C <= 0.5
+     * where...
+     *   ...Φ is the original density function (i.e. the {@link ImageData},
+     *   ...w is a weight controlling the strength of the contouring effect (w ∈ [0, 1])
+     *   ...C is a quantized version of Φ
+     *   ...G_dxd is a Gaussian blur kernel with radius d
+     *
+     * See: Görtler et al. Stippling of 2D Scalar Fields
+     *
+     * @param imageData an {@link ImageData} instance (e.g. from {@link CanvasImageData.getImageData}).
+     * @param quantization a positive integer (quantization will be equal steps of size 1/quantization) or an array of quantization steps
+     * @param weight (optional) a float in range [0,1], determines the strength of the contours. Default: 0.5
+     * @param blurRadius (optional) the radius of the Gaussian blur applied to imageData. Default: 4
+     * @param rgbaToDensity (optional) maps 4 positive integers (r,g,b,a) to a float value. Default: {@link rgbaToLuminance}
+     * @param debugDiv (optional) a string, the id of a div that should be used to display intermediate results. Default: undefined
+     * @return {DensityFunction2D}
+     */
+    static machBandingFromImageData2D(imageData, quantization = 5, weight = 0.5, blurRadius = 4, rgbaToDensity = rgbaToLuminance, debugDiv = undefined) {
+        if (!Array.isArray(quantization)) {
+            quantization = createQuantization(quantization);
+        }
+
+        const {data: originalData} = imageDataToFloatArray(imageData, rgbaToDensity);
+
+        // C (quantize image)
+        const quantizedData = quantize(originalData, quantization);
+
+        // G_dxd * C (blur image)
+        const blurSize = Math.pow(blurRadius, 2.0) * Math.PI;
+        const quantizedImage = floatArrayToImageData(quantizedData, imageData.width);
+        const offscreenCanvas = new OffscreenCanvas(imageData.width, imageData.height);
+        const blurringContext = offscreenCanvas.getContext('2d');
+        blurringContext.putImageData(quantizedImage, 0, 0);
+        blurringContext.filter = `blur(${blurSize}px)`;
+        blurringContext.drawImage(offscreenCanvas, 0, 0);
+        const blurredImage = blurringContext.getImageData(0, 0, imageData.width, imageData.height);
+        const {data: blurredData} = imageDataToFloatArray(blurredImage);
+
+        const deltaData = [];
+        const machBanded = [];
+        for (let i = 0; i < imageData.width * imageData.height; ++i) {
+            // Note: in the paper the weight is only doubled and depending on its value either 0.5 or 1 is substracted
+            // frm delta. However, our delta values are in a different range
+            machBanded.push(clamp(originalData[i] + 4 * weight * (quantizedData[i] - blurredData[i])));
+
+            if (debugDiv) {
+                deltaData.push(quantizedData[i] - blurredData[i]);
             }
         }
-        return new DensityFunction2D(data.map(p => p / maxDensity), imageData.width);
+
+        // debug: show all stages
+        if (debugDiv) {
+            showImage(quantizedImage, debugDiv);
+            showImage(blurredImage, debugDiv);
+            showImage(floatArrayToImageData(deltaData, imageData.width, true), debugDiv);
+            showImage(floatArrayToImageData(machBanded, imageData.width), debugDiv);
+        }
+
+        return new DensityFunction2D(machBanded, imageData.width);
     }
 }
 
-const splitCell = (cell) => {
-    const centroid = d3.polygonCentroid(cell);
-    let largestDir = [0, 0];
-    let secondLargestDir = [0, 0];
-    let largestDistance = 0;
-    let secondLargestDistance = 0;
-    for (const p of d3.polygonHull(cell)) {
-        const direction = [
-            p[0] - centroid[0],
-            p[1] - centroid[1]
-        ];
-        const squaredDistance = direction.reduce((acc, c) => acc + Math.pow(c, 2), 0);
-        if (squaredDistance > largestDistance) {
-            secondLargestDistance = largestDistance;
-            secondLargestDir = largestDir;
-            largestDistance = squaredDistance;
-            largestDir = direction;
-        }
+/**
+ * Not implemented (yet?)
+ *
+ * For restricted stippling contours have to be defined manually
+ */
+class RestrictedStipplingDensityFunction2D extends DensityFunction2D {
+    constructor() {
+        super([], 1);
+        throw new Error('Not implemented');
     }
-    return {
-        position1: [
-            centroid[0] + largestDir[0] * 0.5,
-            centroid[1] + largestDir[1] * 0.5
-        ],
-        position2: [
-            centroid[0] + secondLargestDir[0] * 0.5,
-            centroid[1] + secondLargestDir[1] * 0.5
-        ]
-    };
-};
+}
+
 
 /**
+ * Creates stipples for a given target density function ({@link DensityFunction2D}) using the algorithm presented by
+ * Görtler et al. (Stippling of 2D Scalar Fields).
  *
- * @param targetDensityFunction an instance of {@link DensityFunction2D}
+ * For contouring techniques use a {@link RestrictedStipplingDensityFunction2D} instead of a plain {@link DensityFunction2D}.
+ *
+ * Note that the function returns a {@link Promise}, so you either have to await it in an async context or use its
+ * return values in a {@link Promise} chain.
+ *
+ * @param targetDensityFunction a {@link DensityFunction2D}
  * @param stippleRadius
- * @param initialErrorThreshold
- * @param thresholdConvergenceRate
- * @param numInitialStipples positive integer
- * @param restrictedStippling boolean
- * @param machBanding boolean
- * @returns {any[]}
+ * @param initialErrorThreshold the initial error threshold used for splitting and merging stipples.
+ * @param thresholdConvergenceRate the error threshold is increased by this amount in every iteration
+ * @return {Promise<{voronoi: *, stipples: any[]}>}
  */
-const stipple = async (targetDensityFunction, stippleRadius = 5.0, initialErrorThreshold = 0.0, thresholdConvergenceRate = 0.01, numInitialStipples = 100, restrictedStippling = false, machBanding = false) => {
+const stipple = async (targetDensityFunction, stippleRadius = 5.0, initialErrorThreshold = 0.0, thresholdConvergenceRate = 0.01) => {
     /* Görtler et al:
      *   Input: target density function PHI
      *   Output: list of stipples S
@@ -168,17 +367,22 @@ const stipple = async (targetDensityFunction, stippleRadius = 5.0, initialErrorT
      *   For discrete scalar fields the integral over Vs is just the sum(PHI(i)), for all points i in Vs.
      */
 
+    // keep track of computation time for indiviual steps
     const now = Date.now();
     let timeVoronoi = 0;
     let timeDensity = 0;
     let timeStipples = 0;
 
+    const stippleArea = Math.pow(stippleRadius, 2) * Math.PI;
+    const numInitialStipples = Math.round(
+        ((targetDensityFunction.width * targetDensityFunction.height) / stippleArea) * 0.7);
+
+    let lastVoronoi = null;
     let stipples = Stipple.createRandomStipples(
         numInitialStipples,
         targetDensityFunction.width,
         targetDensityFunction.height);
 
-    const stippleArea = Math.pow(stippleRadius, 2) * Math.PI;
     let errorThreshold = initialErrorThreshold;
     let splitsOrMerges = false;
     do {
@@ -191,18 +395,12 @@ const stipple = async (targetDensityFunction, stippleRadius = 5.0, initialErrorT
         const endVoronoi = Date.now();
 
         const startDensity = Date.now();
-        if (restrictedStippling) {
-        } else if (machBanding) {
-        } else {
-            // to compute the target density for each stipple, we can...
-            //   - ...iterate over the stipples/Voronoi cells and find the density for the region
-            //   - ...iterate over the target density function, find its corresponding stipple/Voronoi cell and add the current value to its density
-            // the former is harder to implement (map the Voronoi cell to discrete indices) but could be done in parallel
-            // the latter is easier to implement, so we'll stick to that for now
-
-            // TODO: this takes up about 85% of the time
-            stipples = targetDensityFunction.assignDensity(stipples, voronoi);
-        }
+        // to compute the target density for each stipple, we can...
+        //   - ...iterate over the stipples/Voronoi cells and find the density for the region
+        //   - ...iterate over the target density function, find its corresponding stipple/Voronoi cell and add the current value to its density
+        // the former is harder to implement (map the Voronoi cell to discrete indices) but could be done in parallel
+        // the latter is easier to implement, so we'll stick to that for now
+        stipples = targetDensityFunction.assignDensity(stipples, voronoi);
         const endDensity = Date.now();
 
         const startStipples = Date.now();
@@ -228,16 +426,26 @@ const stipple = async (targetDensityFunction, stippleRadius = 5.0, initialErrorT
         const endStipples = Date.now();
 
         stipples = nextStipples;
+        lastVoronoi = voronoi;
         errorThreshold += thresholdConvergenceRate;
 
         timeVoronoi += endVoronoi-startVoronoi;
         timeDensity += endDensity-startDensity;
         timeStipples += endStipples-startStipples;
     } while (splitsOrMerges);
+
+    // timing
     const timeTotal = Date.now() - now;
-    console.log('total', timeTotal);
-    console.log('timeVoronoi', timeVoronoi, timeVoronoi / timeTotal);
-    console.log('timeDensity', timeDensity, timeDensity / timeTotal);
-    console.log('timeStipples', timeStipples, timeStipples / timeTotal);
-    return stipples;
+    console.debug(`Total time spent: ${timeTotal} ms`);
+    console.debug(`Time spent building Voronoi diagrams: ${timeVoronoi} ms, ${(timeVoronoi / timeTotal) * 100} %`);
+    console.debug(`Time spent integrating over Voronoi cells: ${timeDensity} ms, ${(timeDensity / timeTotal) * 100} %`);
+    console.debug(`Time spent moving, merging and splitting stipples: ${timeStipples} ms, ${(timeStipples / timeTotal) * 100} %`);
+
+    // map stipple densities to range [0,1]
+    const maxDensity = stipples.reduce((maxDensity, s) => {
+        return s.density > maxDensity ? s.density : maxDensity;
+    }, 0);
+    stipples.forEach(s => s.density /= maxDensity);
+
+    return {stipples, voronoi: lastVoronoi};
 };
