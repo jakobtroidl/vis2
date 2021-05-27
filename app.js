87c145552aaa0926ba80d0ee0c6dfa282bd269fc
jakob.troidl <jakob.troidl@gmail.com>
2021-05-27 17:16:01 +0200

    preparing for deploy

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-const port = 5000;
 
 app.use(express.static("css"));
 app.use(express.static("data"));
@@ -14,5 +13,7 @@ app.get('/', function (req, res)
     res.render("index");
 })
 
-app.listen(process.env.PORT, process.env.IP);
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
