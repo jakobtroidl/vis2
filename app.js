cc92bfe5d62bc2f8f44cb36a36f776ae8ba668bd
jakob.troidl <jakob.troidl@gmail.com>
2021-05-31 13:19:18 +0200

    renamed directories as specified in task description

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static("css"));
 app.use(express.static("data"));
-app.use(express.static("public"));
+app.use(express.static("src"));
 
+app.set('views', path.join(__dirname, '/bin'));
 app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
